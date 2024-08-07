3cab82378c2f812c8647d39c7848c54f6609f033
CyanRayyan <1dangmax@hdsb.ca>
2024-08-07 16:00:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//Made completely by Tyler Dang
+const textchange = document.getElementbyID ("words");
+function Woo(){
+  alert("You got amazed!");
+  words.textContent = "What did you expect?";}
+  function Back(){
+    words.textContent ="Click The button please"
+  }
